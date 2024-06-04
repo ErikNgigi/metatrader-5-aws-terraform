5ba4374bae41e773502ae485dfa683e2c0f8e11e
ErikNgigi <ericmosesngigi@gmail.com>
2024-06-04 17:02:15 +0300

    fix command dpkg

--- files/shell-scripts/install-metatrader-5.sh
+++ files/shell-scripts/install-metatrader-5.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-sudo apt-get update
+# Enable 32bit architecture on the Ubuntu instance and update
+sudo dpkg --add-architecture i386
+sudo apt-get update -y
 
 # create folder for pepperstone executable
 mkdir /tmp/metatrader/
@@ -7,5 +9,6 @@ mkdir /tmp/metatrader/
 # wget to download the pepperstone file
 wget "https://download.mql5.com/cdn/web/pepperstone.group.limited/mt5/pepperstone5setup.exe" -O /tmp/metatrader/pepperstone.exe
 
-# Install wine both 64bit and 32bit
-sudo apt install wine64 wine32
+# Install wine both 32bit and 64bit version
+sudo apt install wine -y
+sudo apt install wine32 -y
