3ddeced58837af7c8c4e5d8d2e526b7d6f3f5c2a
ErikNgigi <ericmosesngigi@gmail.com>
2024-06-21 14:49:50 +0300

    shell script to install wine

--- infrastructure/modules/instances/main.tf
+++ infrastructure/modules/instances/main.tf
@@ -5,6 +5,11 @@ resource "aws_instance" "metatrader_instance_1" {
   subnet_id       = var.metatrader_public_subnet_1
   key_name        = var.metatrader_ssh_key
   user_data       = file("${path.module}/scripts/extras.sh")
+  ebs_block_device {
+    device_name = "/dev/sda1"
+    volume_size = 20
+    volume_type = "gp3"
+  }
 
   tags = {
     Name = var.instance_name
--- infrastructure/modules/instances/scripts/extras.sh
+++ infrastructure/modules/instances/scripts/extras.sh
@@ -1,21 +1,33 @@
 #!/usr/bin/env bash
 
-# update packages
+# Disable prompt during installation
+export DEBAIN_FRONTEND=noninteractive
+echo "debconf debconf/frontend select noninteractive" | sudo debconf-set-selections
+
+# Add sources to ubuntu instances
+sudo sed -i -E 's/^# deb-src /deb-src /g' /etc/apt/sources.list
 sudo dpkg --add-architecture i386
+
+# Add packages
 sudo apt update -y
+sudo apt install openssh-server sudo xrdp xorgxrdp xfce4 xfce4-terminal xdg-user-dir vim wget -y
 
-# set timezone
-sudo timedatectl set-timezone Africa/Nairobi
-sudo timedatectl set-ntp on
+# setup ssh x11 forwarding
+sudo sed -i 's/^#   ForwardX11 no/   ForwardX11 yes/g' /etc/ssh/ssh_config
+sudo sed -i 's/^#X11Forwarding yes/X11Forwarding yes/g' /etc/ssh/sshd_config
+sudo sed -i 's/^#X11DisplayOffset 10/X11DisplayOffset 10/g' /etc/ssh/sshd_config
+sudo sed -i 's/^#X11DisplayOffset 10/X11DisplayOffset 10/g' /etc/ssh/sshd_config
 
-# install prerequesite packages
-sudo apt install -y --no-install-recommends curl firefox git unzip wget vim
-
-# install wine prerequesites
+# Install winestable
 sudo mkdir -pm755 /etc/apt/keyrings
 sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
-sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/focal/winehq-focal.sources
-
-# install wine-stable
-sudo apt update -y
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
+sudo apt-get update -y
 sudo apt install --install-recommends winehq-stable -y
+
+# Start xrdp for remote desktop
+sudo service xrdp start
+sudo service xrdp restart
+
+# Create user directories
+sudo xdg-user-dirs-update
